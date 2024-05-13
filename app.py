b3c549f02a1dd18039762f3eedd1ce8a59602aa7
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-05-13 23:12:57 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/get_mp4', methods=['POST'])
-def get_mp4():
+@app.route('/get_mp4_mp3', methods=['POST'])
+def get_mp4_mp3():
     youtube_link = request.form['youtube_link']
     try:
         yt = YouTube(youtube_link)
         mp4_url = yt.streams.filter(file_extension='mp4').first().url
-        return render_template('result.html', mp4_url=mp4_url)
+        mp3_url = yt.streams.filter(only_audio=True).first().url
+        return render_template('result.html', mp4_url=mp4_url, mp3_url=mp3_url)
     except Exception as e:
         error_message = str(e)
         return render_template('error.html', error_message=error_message)
