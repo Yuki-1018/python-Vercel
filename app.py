6773712d57b99b6ed8c0076d369e24827c0d0db3
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-05-23 15:07:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,73 @@
 from flask import Flask, render_template, request
-from pytube import YouTube
+from flask_socketio import SocketIO, join_room, leave_room, emit, disconnect
+import time
+import threading
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+rooms = {}
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get_mp4_mp3', methods=['POST'])
-def get_mp4_mp3():
-    youtube_link = request.form['youtube_link']
-    try:
-        yt = YouTube(youtube_link)
-        mp4_url = yt.streams.filter(file_extension='mp4').first().url
-        mp3_url = yt.streams.filter(only_audio=True).first().url
-        return render_template('result.html', mp4_url=mp4_url, mp3_url=mp3_url)
-    except Exception as e:
-        error_message = str(e)
-        return render_template('error.html', error_message=error_message)
+def countdown(room):
+    for i in range(5, 0, -1):
+        socketio.emit('countdown', {'count': i}, room=room)
+        time.sleep(1)
+    socketio.emit('start_game', room=room)
+
+@socketio.on('create_room')
+def on_create(data):
+    room = data['room']
+    if room not in rooms:
+        join_room(room)
+        rooms[room] = {'players': [request.sid], 'status': 'waiting'}
+        emit('room_created', {'room': room})
+    else:
+        emit('room_exists', {'room': room})
+
+@socketio.on('join_room')
+def on_join(data):
+    room = data['room']
+    if room in rooms and len(rooms[room]['players']) < 2:
+        join_room(room)
+        rooms[room]['players'].append(request.sid)
+        if len(rooms[room]['players']) == 2:
+            rooms[room]['status'] = 'countdown'
+            threading.Thread(target=countdown, args=(room,)).start()
+        emit('room_joined', {'room': room})
+    elif room in rooms and len(rooms[room]['players']) >= 2:
+        emit('room_full', {'room': room})
+    else:
+        emit('room_not_found', {'room': room})
+
+@socketio.on('update')
+def on_update(data):
+    room = data['room']
+    emit('update', data, room=room, include_self=False)
+
+@socketio.on('game_over')
+def on_game_over(data):
+    room = data['room']
+    emit('game_over', room=room)
+    rooms[room]['status'] = 'waiting'
+    rooms[room]['players'] = []
+
+@socketio.on('disconnect')
+def on_disconnect():
+    for room, info in rooms.items():
+        if request.sid in info['players']:
+            info['players'].remove(request.sid)
+            if len(info['players']) == 0:
+                del rooms[room]
+            else:
+                emit('opponent_disconnected', room=room)
+                info['status'] = 'waiting'
+                leave_room(room)
+            break
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
